aa3a74e8aa4e9756c29ed7b4fff5740c5135fa20
Daven Quinn <dev@davenquinn.com>
2020-02-18 14:12:07 -0600

    Added a skeletal docker container config

--- sparrow-config.sh
+++ sparrow-config.sh
@@ -1,4 +1,4 @@
-# Configures environment for WiscSIMS labs
+# Configures environment for WiscSIMS lab
 
 export PROJECT_DIR="${0:h}"
 export SPARROW_PATH="$PROJECT_DIR/Sparrow"
@@ -7,3 +7,6 @@ export SPARROW_LAB_NAME="WiscSIMS"
 # Keep volumes for this project separate from those for different labs
 export COMPOSE_PROJECT_NAME="${SPARROW_LAB_NAME}"
 export SPARROW_SECRET_KEY="test"
+
+export SPARROW_COMPOSE_OVERRIDES="-f importer-container.yaml"
+export SPARROW_COMMANDS="$PROJECT_DIR/bin"
