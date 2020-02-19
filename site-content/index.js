758aa4220b0c927969b44f07f6e08887f66c34d0
Daven Quinn <dev@davenquinn.com>
2020-02-19 17:13:29 -0600

    Add basic site content

--- site-content/index.js
+++ site-content/index.js
@@ -3,5 +3,6 @@ import aboutText from './about-the-lab.md'
 import h from 'react-hyperscript'
 
 export default {
-  landingText: h(Markdown, {src: aboutText})
+  landingText: h(Markdown, {src: aboutText}),
+  siteTitle: process.env.SPARROW_LAB_NAME
 }
