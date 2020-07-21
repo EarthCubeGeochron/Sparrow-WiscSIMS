30a03d90d7b6ae95615b2e7db5993ab8ab5c4eb5
Daven Quinn <dev@davenquinn.com>
2020-07-21 16:04:20 -0500

    Updated Sparrow config

--- sparrow-config.sh
+++ sparrow-config.sh
@@ -1,6 +1,6 @@
 # Configures environment for WiscSIMS lab
 
-export PROJECT_DIR="$(dirname "$(readlink -f "$0")")"
+export PROJECT_DIR="$SPARROW_CONFIG_DIR"
 export SPARROW_PATH="$PROJECT_DIR/Sparrow"
 export SPARROW_LAB_NAME="WiscSIMS"
 
@@ -8,8 +8,14 @@ export SPARROW_LAB_NAME="WiscSIMS"
 export COMPOSE_PROJECT_NAME="${SPARROW_LAB_NAME}"
 export SPARROW_SECRET_KEY="test"
 export SPARROW_HTTP_PORT=80
+export SPARROW_HTTPS_PORT=443
+
+export SPARROW_BACKUP_DIR="${PROJECT_DIR}/db-backups"
 
 export SPARROW_DATA_DIR="${PROJECT_DIR}/Test-Data"
 export SPARROW_COMPOSE_OVERRIDES="-f ${PROJECT_DIR}/importer-container.yaml"
 export SPARROW_COMMANDS="$PROJECT_DIR/bin"
 export SPARROW_SITE_CONTENT="$PROJECT_DIR/site-content"
+
+overrides="$PROJECT_DIR/sparrow-config.overrides.sh"
+[ -f "$overrides" ] && source "$overrides"
