4bece0c48ce3660873448cfa06778ec918fbc5a0
Ben Linzmeier <bjlinzmeier@gmail.com>
2021-06-09 18:22:21 -0500

    Update sparrow-config.sh

--- sparrow-config.sh
+++ sparrow-config.sh
@@ -1,5 +1,7 @@
 # Configures environment for WiscSIMS lab
 
+#export SPARROW_ENV="development"
+
 export PROJECT_DIR="$SPARROW_CONFIG_DIR"
 export SPARROW_PATH="$PROJECT_DIR/Sparrow"
 export SPARROW_LAB_NAME="WiscSIMS"
@@ -7,11 +9,15 @@ export SPARROW_LAB_NAME="WiscSIMS"
 # Keep volumes for this project separate from those for different labs
 export COMPOSE_PROJECT_NAME="${SPARROW_LAB_NAME}"
 export SPARROW_SECRET_KEY="test"
+export SPARROW_HTTP_PORT=80
+export SPARROW_HTTPS_PORT=443
+
+export SPARROW_BACKUP_DIR="${PROJECT_DIR}/db-backups"
 
 export SPARROW_DATA_DIR="${PROJECT_DIR}/Test-Data"
 export SPARROW_COMPOSE_OVERRIDES="${PROJECT_DIR}/importer-container.yaml"
 export SPARROW_COMMANDS="$PROJECT_DIR/bin"
 export SPARROW_SITE_CONTENT="$PROJECT_DIR/site-content"
-export SPARROW_ENV="development"
 
-export MAPBOX_API_TOKEN="pk.eyJ1IjoidGhlZmFsbGluZ2R1Y2siLCJhIjoiY2tsOHAzeDZ6MWtsaTJ2cXhpMDAxc3k5biJ9.FUMK57K0UP7PSrTUi3DiFQ"
+overrides="$PROJECT_DIR/sparrow-config.overrides.sh"
+[ -f "$overrides" ] && source "$overrides"
